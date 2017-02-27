bfa9c07d9d5b1801484f375c5efe7faed50b5780
Matthew Bauer <mjbauer95@gmail.com>
2017-02-26 22:14:08 -0600

    Add authinfo

--- custom.el
+++ custom.el
@@ -10,6 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ag-highlight-search t)
+ '(auth-sources (quote ("~/.authinfo")))
  '(auto-revert-verbose nil)
  '(auto-save-file-name-transforms (\` ((".*" (\, temporary-file-directory) t))))
  '(backup-directory-alist (\` ((".*" \, temporary-file-directory))))
