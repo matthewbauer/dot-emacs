c84a5798a57183825e27d77cf42f3f1e512a348a
Matthew Bauer <mjbauer95@gmail.com>
2017-05-15 15:55:11 -0500

    add personal functions

--- lisp/misc.el
+++ lisp/misc.el
@@ -211,4 +211,51 @@ i.e. change right window to bottom, or change bottom window to right."
 
 (global-set-key (kbd "C-x k") 'bjm/kill-this-buffer)
 
+(defalias 'eldoc-get-fnsym-args-string 'elisp-get-fnsym-args-string)
+
+(global-set-key (kbd "C-x ~") (lambda () (interactive) (dired "~")))
+
+(defun sort-package-declarations ()
+  (interactive)
+  (cl-flet ((next-use-package
+             () (if (re-search-forward "^(use-package " nil t)
+                    (goto-char (match-beginning 0))
+                  (goto-char (point-max)))))
+    (sort-subr
+     nil
+     #'next-use-package
+     #'(lambda ()
+         (goto-char (line-end-position))
+         (next-use-package))
+     #'(lambda ()
+         (re-search-forward "(use-package \\([A-Za-z0-9_+-]+\\)")
+         (match-string 1)))))
+
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
+(define-key ctl-x-4-map "t" 'toggle-window-split)
+
 (provide 'misc)
